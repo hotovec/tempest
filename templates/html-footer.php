654adb0fd782c3f7a76fbe350d785394aaa85cdc
Michal Hotovec <michal.hotovec@gmail.com>
2014-04-28 17:08:36 +0200

    relink bootstrap to bover component

--- templates/html-footer.php
+++ templates/html-footer.php
@@ -5,11 +5,9 @@
 
 <script>
     $("[data-toggle=tooltip]").tooltip();
-
-   $(".offcanavas-menu-left").offcanvas({ toggle: false, disableScrolling: true,canvas:'body' });
-    $(".offcanavas-menu-right").offcanvas({ toggle: false, disableScrolling: false, canvas:'body' });
+    $(".offcanavas-menu-left").offcanvas({ toggle: false, disableScrolling: true, canvas: 'body' });
+    $(".offcanavas-menu-right").offcanvas({ toggle: false, disableScrolling: false, canvas: 'body' });
     $(".offcanavas-menu-bottom").offcanvas({ toggle: false, disableScrolling: false });
-
 </script>
 
 </body>
