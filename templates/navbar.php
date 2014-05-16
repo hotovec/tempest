0b9522538777b7c1be628c19aa7ca868a65be89b
Michal Hotovec <michal.hotovec@gmail.com>
2014-05-16 15:51:57 +0200

    filters add interaction attributes

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,12 +1,24 @@
 <nav class="navbar navbar-fixed-top" role="navigation">
     <div class="container">
+
         <div class="search-tags">
+
+            <!-- search field-->
             <div class="search-field">
-                <div id="searchOptionsButton" class="search-button"><i class="fa fa-angle-down"></i> <i
-                        class="fa fa-search"></i></div>
-                <input class="search" type="text" name="" id="mytags" placeholder="presets"
-                    value="Articles,Products,Ready for Edit,Last Week" data-role="tagsinput">
+
+                <div id="searchOptionsButton" class="search-button">
+                    <i class="fa fa-angle-down"></i> <i
+                        class="fa fa-search"></i>
+                </div>
+
+                <input class="search" type="text" name="" id="searchtags"
+                    placeholder="presets"
+                    value="Articles,Products,Ready for Edit,Last Week"
+                    data-role="tagsinput">
+
             </div>
+
+            <!-- expandable search options-->
             <div class="search-tags--options hide">
 
                 <div class="row">
@@ -14,7 +26,9 @@
                         <div><i class="fa fa-star"></i> starred</div>
                     </div>
                 </div>
+
                 <div class="row">
+
                     <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
                         <h4>content type</h4>
 
@@ -27,6 +41,7 @@
                         <div><i class="fa fa-link"></i> link</div>
                         <div><i class="fa fa-tag"></i> rich term</div>
                     </div>
+
                     <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
                         <h4>Authoring status</h4>
 
@@ -34,6 +49,7 @@
                         <div><i class="fa fa-adjust"></i> ready for edit</div>
                         <div><i class="fa fa-circle"></i> complete</div>
                     </div>
+
                     <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
                         <h4>Publication status</h4>
 
@@ -41,6 +57,7 @@
                         <div><i class="fa fa-circle color-scheduled"></i> scheduled</div>
                         <div><i class="fa fa-circle color-published"></i> published</div>
                     </div>
+
                     <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
                         <h4>Date range</h4>
 
@@ -51,9 +68,7 @@
                                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><i
                                         class="fa fa-calendar"></i></button>
                                 <ul class="dropdown-menu pull-right">
-                                    <li><a href="#">add calednar here</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
+                                    <li><a href="#">add calednar widget here</a></li>
                                 </ul>
                             </div>
                             <!-- /btn-group -->
@@ -65,21 +80,25 @@
                 </div>
 
             </div>
+            <!-- expandable search options : end-->
+
         </div>
     </div>
+
     <div class="container">
-        <div class="row content-management-presets">
+        <div class="row">
             <div class="col-sm-12">
+
                 <div id="filter-pillbox" class="pillbox navbar">
-                    <button type="button" class="add-filter" class="btn btn-default">Default <i class="fa fa-times-circle"></i></button>
-                    <button type="button" class="btn btn-success">Success <i class="fa fa-times-circle"></i></button>
-                    <button type="button" class="btn btn-info">Info <i class="fa fa-times-circle"></i></button>
-                    <button type="button" class="btn btn-warning">Warning <i class="fa fa-times-circle"></i></button>
-                    <button type="button" class="btn btn-danger">Danger <i class="fa fa-times-circle"></i></button>
+                    <button data-toggle="filter-add" data-filter-name="Success" type="button" class="btn btn-success">Success <i class="fa fa-times-circle"></i></button>
+                    <button data-toggle="filter-add" data-filter-name="Info" type="button" class="btn btn-info">Info <i class="fa fa-times-circle"></i></button>
+                    <button data-toggle="filter-add" data-filter-name="Warning" type="button" class="btn btn-warning">Warning <i class="fa fa-times-circle"></i></button>
+                    <button data-toggle="filter-add" data-filter-name="Danger" type="button" class="btn btn-danger">Danger <i class="fa fa-times-circle"></i></button>
                     <!-- add new filter button-->
                     <button type="button" class="btn btn-primary">Add new filter <i class="fa fa-plus-circle"></i>
                     </button>
                 </div>
+
             </div>
         </div>
     </div>
