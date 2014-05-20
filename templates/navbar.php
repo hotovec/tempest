37a502c8a2362e9e063e2383b1dd3ecee65bcf96
Michal Hotovec <michal.hotovec@gmail.com>
2014-05-20 12:33:04 +0200

    fix header style (~ v1.13) - option boxes

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,94 +1,89 @@
 <nav class="navbar" role="navigation">
 
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="search-tags">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="search-tags">
 
-            <!-- search field-->
-            <div class="search-field">
+                <!-- expandable search options-->
+                <div class="search-tags--options hide">
 
-                <div id="searchOptionsButton" class="search-button">
-                    <i class="fa fa-angle-down"></i> <i
-                        class="fa fa-search"></i>
-                </div>
-
-                <input class="search" type="text" name="" id="searchtags"
-                    placeholder="Search for content"
-                    data-role="tagsinput">
-
-            </div>
-
-            <!-- expandable search options-->
-            <div class="search-tags--options hide">
-
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <div><i class="fa fa-star"></i> starred</div>
-                    </div>
-                </div>
-
-                <div class="row">
-
-                    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                        <h4>content type</h4>
-
-                        <div><i class="fa fa-file-text-o"></i> article</div>
-                        <div><i class="fa fa-shopping-cart"></i> product</div>
-                        <div><i class="fa fa-user-md"></i> author profile</div>
-                        <div><i class="fa fa-user"></i> person</div>
-                        <div><i class="fa fa-file"></i> basic page</div>
-                        <div><i class="fa fa-bar-chart-o"></i> data</div>
-                        <div><i class="fa fa-link"></i> link</div>
-                        <div><i class="fa fa-tag"></i> rich term</div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                        <h4>Authoring status</h4>
-
-                        <div><i class="fa fa-circle-o"></i> draft</div>
-                        <div><i class="fa fa-adjust"></i> ready for edit</div>
-                        <div><i class="fa fa-circle"></i> complete</div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                        <h4>Publication status</h4>
-
-                        <div><i class="fa fa-circle color-unpublished"></i> unpublished</div>
-                        <div><i class="fa fa-circle color-scheduled"></i> scheduled</div>
-                        <div><i class="fa fa-circle color-published"></i> published</div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                        <h4>Date range</h4>
-
-                        <div class="input-group">
-                            <input type="text" class="form-control">
-
-                            <div class="input-group-btn">
-                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><i
-                                        class="fa fa-calendar"></i></button>
-                                <ul class="dropdown-menu pull-right">
-                                    <li><a href="#">add calednar widget here</a></li>
-                                </ul>
-                            </div>
-                            <!-- /btn-group -->
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <div><i class="fa fa-star"></i> starred</div>
                         </div>
-                        <!-- /input-group -->
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <h4>content type</h4>
+
+                            <div><i class="fa fa-file-text-o"></i> article</div>
+                            <div><i class="fa fa-shopping-cart"></i> product</div>
+                            <div><i class="fa fa-user-md"></i> author profile</div>
+                            <div><i class="fa fa-user"></i> person</div>
+                            <div><i class="fa fa-file"></i> basic page</div>
+                            <div><i class="fa fa-bar-chart-o"></i> data</div>
+                            <div><i class="fa fa-link"></i> link</div>
+                            <div><i class="fa fa-tag"></i> rich term</div>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <h4>Authoring status</h4>
+
+                            <div><i class="fa fa-circle-o"></i> draft</div>
+                            <div><i class="fa fa-adjust"></i> ready for edit</div>
+                            <div><i class="fa fa-circle"></i> complete</div>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <h4>Publication status</h4>
+
+                            <div><i class="fa fa-circle color-unpublished"></i> unpublished</div>
+                            <div><i class="fa fa-circle color-scheduled"></i> scheduled</div>
+                            <div><i class="fa fa-circle color-published"></i> published</div>
+                        </div>
+
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <h4>Date range</h4>
+
+                            <div class="input-group">
+                                <input type="text" class="form-control">
+
+                                <div class="input-group-btn">
+                                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                                        <i class="fa fa-calendar"></i></button>
+                                    <ul class="dropdown-menu pull-right">
+                                        <li><a href="#">add calednar widget here</a></li>
+                                    </ul>
+                                </div>
+                                <!-- /btn-group -->
+                            </div>
+                            <!-- /input-group -->
+
+                        </div>
 
                     </div>
 
                 </div>
+                <!-- expandable search options : end-->
+
+                <!-- search field-->
+                <div class="search-field">
+
+                    <div id="searchOptionsButton" class="search-button">
+                        <i class="fa fa-angle-down"></i> <i class="fa fa-search"></i>
+                    </div>
+
+                    <input class="search" type="text" name="" id="searchtags" placeholder="Search for content" data-role="tagsinput">
+                </div>
 
             </div>
-            <!-- expandable search options : end-->
-
         </div>
-            </div>
-        </div>
+    </div>
 
 
-
-        <div class="row">
+    <div class="row">
 
             <div class="col-sm-12">
 
@@ -98,7 +93,7 @@
                     <button data-toggle="filter-add" data-filter-name="Warning" type="button" class="btn btn-warning">Warning <i class="fa fa-times-circle"></i></button>
                     <button data-toggle="filter-add" data-filter-name="Danger" type="button" class="btn btn-danger">Danger <i class="fa fa-times-circle"></i></button>
                     <!-- add new filter button-->
-                    <button type="button" class="btn btn-primary">Add new filter <i class="fa fa-plus-circle"></i>
+                    <button type="button" class="btn btn-primary">Add shortcut <i class="fa fa-plus-circle"></i>
                     </button>
                 </div>
 
