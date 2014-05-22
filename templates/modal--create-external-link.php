babba2475fe7d2f77a7980f6d68038d06be8b1cf
Michal Hotovec <michal.hotovec@gmail.com>
2014-05-22 14:58:26 +0200

    modal create external-link

---
+++ templates/modal--create-external-link.php
@@ -0,0 +1,54 @@
+<div class="pt-layer modal-fullscreen modal-extlink-create">
+    <div class="modal-body">
+        <form action="" method="POST" role="form">
+            <div class="modal-body-content center">
+
+                <div class="modal--hed">Create an Internal/External Link</div>
+
+
+            </div>
+            <div class="container">
+                <div class="col-sm-8 col-sm-push-2">
+                    <div class="panel panel-default">-
+                        <div class="panel-body">
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label for="ulr"></label>
+                                        <input type="text" class="form-control" name="" id="" placeholder="Enter complete url link ...">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                    <img src="#" class="img-responsive" alt="Image">
+                                </div>
+                                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="" id="" placeholder="Enter complete url link ...">
+                                    </div>
+                                    <div class="form-group">
+                                        <textarea name="" id="" rows="6" style="width:100%;"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-cancel" data-layer="dimmis-all">remove</button>
+                <button type="button" class="btn btn-cancel" data-layer="dimmis-all">cancel</button>
+                <button type="button" class="btn btn-action" data-layer="dimmis-all">save</button>
+            </div>
+        </form>
+    </div>
+
+</div>
+
